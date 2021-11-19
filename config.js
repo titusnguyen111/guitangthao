c536a31529e04f8d8840e710345134b5011698e5
titusnguyen111 <huudat890@gmail.com>
2021-11-19 10:11:04 +0700

    init file

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Thảo yêu dấu',
+    introDesc: `Tui có điều này muốn hỏi đó nha`,
+    btnIntro: 'Oke',
+    title: 'Cậu nợ tui 1 bữa ăn sáng',
+    desc: 'Hãy chọn 1 trong 2 thứ dưới đây ',
+    btnYes: 'Beef Wellington',
+    btnNo: 'Hủ Tiếu',
+    question:'Hỏi chân thành cậu có xài filter không ???',
+    btnReply: 'Oke',
+    reply: 'Tớ xài rất nhiều đó nha <333',
+    mess: 'Tớ biết mà',
+    messDesc: '',
+    btnAccept: 'Còn chờ gì nữa mà không chốt quán ăn <333',
+    messLink: 'https://www.facebook.com/CucThanBistro/posts/483092825870901/' //
+}
